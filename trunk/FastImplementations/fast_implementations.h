018934133d874dd1a2311b98712b1a6f92f9277b
pasha.e <pasha.e@6a50d66c-e292-e5f9-22cc-139ecedfa042>
2010-05-28 13:05:23 +0000

    Added source code for mesh reduction, and also moved djkstra implementation to separate cpp file, in order to shorten compilation time.

--- trunk/FastImplementations/fast_implementations.h
+++ trunk/FastImplementations/fast_implementations.h
@@ -1,3 +1,9 @@
+#ifndef _FAST_IMPLEMENTATIONS_H_
+#define _FAST_IMPLEMENTATIONS_H_
 
 void __stdcall computeGeodesic(double* distances, const double* vertices, 
 								 const int* faces, int nvertices, int nfaces);
+
+int __stdcall reduceMesh(double* vertices, int* faces, int nvertices, int nfaces, int reducedVertices);
+
+#endif
