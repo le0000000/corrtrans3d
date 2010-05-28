c47f1f1f3dcb770f5055848a3da531e95a705ccc
pasha.e <pasha.e@6a50d66c-e292-e5f9-22cc-139ecedfa042>
2010-05-28 12:14:29 +0000

    Added Test project for FastImplementations.dll.

---
+++ FastImplementations/Test/Mesh.cpp
@@ -0,0 +1,43 @@
+#include "Mesh.h"
+#include <stdexcept>
+
+template <typename T, typename F>
+void tokenize(char* s, T* values, size_t maxCount, F converter) {
+	char* tok = strtok(s, " ");
+	size_t nTokens = 0;
+
+	while (tok && (nTokens < maxCount)) {
+		values[nTokens] = converter(tok);
+		nTokens++;
+		tok = strtok(NULL, " ");
+	}
+}
+
+Mesh::Mesh(const std::string& smfFilename) {
+
+	FILE* file = fopen(smfFilename.c_str(), "r");
+	if (!file) {
+		throw std::invalid_argument("Failed to open file for reading");
+	}
+
+	char line[256];
+	int ret = fscanf(file, "%[^\n]", line);
+	while (ret > 0) {
+
+		if (line[0] == 'v') {
+			Vertex v;
+			tokenize(line + 2, &v.x, 3, atof);
+			m_vertices.push_back(v);
+		} else if (line[0] == 'f') {
+			Face f;
+			tokenize(line + 2, f.vertices, 3, atoi);
+			m_faces.push_back(f);
+		}
+
+		getc(file);
+		ret = fscanf(file, "%[^\n]", line);
+	}
+
+	fclose(file);
+
+}
