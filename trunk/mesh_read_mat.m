5099a99b253095018edcfa04bceb0520307c3cea
pasha.e <pasha.e@6a50d66c-e292-e5f9-22cc-139ecedfa042>
2010-06-15 18:40:04 +0000

    - added Reyleigh quotient iteration (not yet integrated with the weights method)  - added coordinate normalization for mesh_read_mat.m  - removed lots of annoying printouts

--- trunk/mesh_read_mat.m
+++ trunk/mesh_read_mat.m
@@ -4,3 +4,10 @@ function [mesh] = mesh_read_mat(filename)
 	
 	mesh.faces = model.surface.TRIV;
 	mesh.vertices = [model.surface.X model.surface.Y model.surface.Z];
+
+    % normalize the vertex coordinates from [-Inf, Inf] to [0,1]
+    
+    min_val = min(mesh.vertices(:));
+    max_val = max(mesh.vertices(:));
+    
+    mesh.vertices = (mesh.vertices - min_val) ./ (max_val - min_val);
